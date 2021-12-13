20fda741ea88675e9416b9caa321a8aca2e8f51c
Bùi Ngọc Lâm <74352628+ngoclam9200@users.noreply.github.com>
2021-12-13 11:13:51 +0700

    .

--- src/app/bill/bill.component.ts
+++ src/app/bill/bill.component.ts
@@ -17,14 +17,15 @@ import { CreatebillComponent } from '../createbill/createbill.component';
 export class BillComponent implements OnInit {
 data:any=[]
 nobill=true
+remove=true
   constructor(private http:HttpClient, private api:ApiService) { }
 
 
   ngOnInit(): void {
-    this.currentData()
+    this.allbill()
   }
 
-  currentData() {
+  allbill() {
 
 
 
@@ -66,14 +67,53 @@ nobill=true
 
 
   }
+  waitforconfirm()
+  {
+    let headers = new HttpHeaders();
+    var currentUser = JSON.parse(localStorage.getItem('currentUser') || '{}');
+    var token = currentUser.token; // your token
+     headers = headers.set('Access-Control-Allow-Origin', '*').set('Authorization', `Bearer ${token}`);
+
+
+
+    this.http.get(this.api.apibill+`Xemhoadontheotrangthainguoidung/5`, { headers: headers }).subscribe(res => {
+       this.data = res
+       this.data=this.data.data
+     
+       console.log(this.data)
+      
+       if(this.data,length==0) this.nobill=true
+       else 
+       {this.nobill=false
+      
+      
+       }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    });
+
+  }
   deletebill(id)
   {
     let headers = new HttpHeaders();
     var currentUser = JSON.parse(localStorage.getItem('currentUser') || '{}');
     var token = currentUser.token; // your token
      headers = headers.set('Access-Control-Allow-Origin', '*').set('Authorization', `Bearer ${token}`);
-     this.http.put(this.api.apibill+`nguoidunghuydonhang/` + id, { headers: headers }).subscribe(res => {
+     var a={}
+     this.http.put(this.api.apibill+`nguoidunghuydonhang/` +id,a, { headers: headers }).subscribe(res => {
       console.log(res)
+       this.allbill()
       
      
   
