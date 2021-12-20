d2b4eec9ed467e987d1f9e18665bd0a78fe392a0
Bùi Ngọc Lâm <74352628+ngoclam9200@users.noreply.github.com>
2021-12-20 10:24:16 +0700

    fix bug report

--- src/app/createbill/createbill.component.ts
+++ src/app/createbill/createbill.component.ts
@@ -98,78 +98,111 @@ data:any=[]
     var currentUser = JSON.parse(localStorage.getItem('currentUser') || '{}');
     var token = currentUser.token; // your token
      headers = headers.set('Access-Control-Allow-Origin', '*').set('Authorization', `Bearer ${token}`);
+     const sdt=this.formGroup.controls['sdtNguoiNhan'].value;
+     const diaChiGiaoHang=this.formGroup.controls['diaChiGiaoHang'].value;
+     var number = "0123456789"
+     var checksdt = function (string) {
+      for (let i = 0; i < string.length; i++) {
+        if (number.indexOf(string[i]) == -1) {
+          return true
+        }
+      }
+      return false;
+    }
+    var specialChars = "<>@!#$%^&*()_+[]{}?:;|'\"\\,./~`-="
+
+    var checkaddress = function (string) {
+      for (let i = 0; i < specialChars.length; i++) {
+        if (string.indexOf(specialChars[i]) > -1) {
+          return true
+        }
+      }
+      return false;
+    }
+
+
+    if (checkaddress(diaChiGiaoHang) == true) {
+
+      return alert('Address không hợp lệ');
+    }
+
+
+    if (checksdt(sdt) == true) {
+
+      return alert('Phone number receiver không hợp lệ');
+    }
 
 
 
-    this.http.get(this.api.apiorder+`xemgiohang`, { headers: headers }).subscribe(res => {
+    // this.http.get(this.api.apiorder+`xemgiohang`, { headers: headers }).subscribe(res => {
      
-      this.tmp=res
-      this.tmp=this.tmp.data[0]
-      console.log(this.tmp)
+    //   this.tmp=res
+    //   this.tmp=this.tmp.data[0]
+    //   console.log(this.tmp)
       
     
-      for(let i=0 ;i<this.tmp.length;i++)
-      { 
-        var a={sanPhamId: this.tmp[i].sanPhamId, soLuongDat:this.tmp[i].soLuongTrongGio}
-        this.array.push(a)
+    //   for(let i=0 ;i<this.tmp.length;i++)
+    //   { 
+    //     var a={sanPhamId: this.tmp[i].sanPhamId, soLuongDat:this.tmp[i].soLuongTrongGio}
+    //     this.array.push(a)
 
-      }
-           this.formGroup = new FormGroup({
-         danhSachDat: new FormControl(this.formArray.controls),
+    //   }
+    //        this.formGroup = new FormGroup({
+    //      danhSachDat: new FormControl(this.formArray.controls),
       
-        diaChiGiaoHang: new FormControl(this.formGroup.controls['diaChiGiaoHang'].value, [Validators.required]),
-        sdtNguoiNhan: new FormControl(this.formGroup.controls['sdtNguoiNhan'].value, [Validators.required]),
-        thanhToanOnline: new FormControl(false, [Validators.required]),
+    //     diaChiGiaoHang: new FormControl(this.formGroup.controls['diaChiGiaoHang'].value, [Validators.required]),
+    //     sdtNguoiNhan: new FormControl(this.formGroup.controls['sdtNguoiNhan'].value, [Validators.required]),
+    //     thanhToanOnline: new FormControl(false, [Validators.required]),
        
-      });
+    //   });
       
-      if (this.formGroup.valid)
-      {
-        Swal.fire({
-          title: 'Are you sure?',
-          text: "Create bill!",
-          icon: 'warning',
-          showCancelButton: true,
-          confirmButtonColor: '#3085d6',
-          cancelButtonColor: '#d33',
-          confirmButtonText: 'Yes,create bill!'
-        }).then((result) => {
-          if (result.isConfirmed) {
-            this.http.post(this.api.apibill+`taohoadon` ,this.formGroup.value, { headers: headers }).subscribe(res => {
+    //   if (this.formGroup.valid)
+    //   {
+    //     Swal.fire({
+    //       title: 'Are you sure?',
+    //       text: "Create bill!",
+    //       icon: 'warning',
+    //       showCancelButton: true,
+    //       confirmButtonColor: '#3085d6',
+    //       cancelButtonColor: '#d33',
+    //       confirmButtonText: 'Yes,create bill!'
+    //     }).then((result) => {
+    //       if (result.isConfirmed) {
+    //         this.http.post(this.api.apibill+`taohoadon` ,this.formGroup.value, { headers: headers }).subscribe(res => {
            
-              this.tmp=res
+    //           this.tmp=res
             
               
              
           
         
-           });
-           setTimeout(() => {
-             window.location.reload()
-           }, 3000);
+    //        });
+    //        setTimeout(() => {
+    //          window.location.reload()
+    //        }, 3000);
            
-            Swal.fire(
-              'Success!',
-              '',
-              'success'
+    //         Swal.fire(
+    //           'Success!',
+    //           '',
+    //           'success'
               
     
-            )
+    //         )
           
            
-          }
+    //       }
     
     
-        })
+    //     })
     
 
         
       
-      }
-      else 
-      alert("Please fill the form")
+    //   }
+    //   else 
+    //   alert("Please fill the form")
     
-    });
+    // });
     //   console.log(this.array)
     //  this.formArray.setValue(this.array)
 
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient ,HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { FormControl, Validators } from '@angular/forms';
 
 import { FormGroup } from '@angular/forms';
@@ -15,7 +15,7 @@ import { ApiService } from 'src/services/api.service';
 export class SignupComponent implements OnInit {
   formGroup: FormGroup;
   constructor(private http: HttpClient,
-    private router: Router, private api:ApiService) { }
+    private router: Router, private api: ApiService) { }
 
   ngOnInit(): void {
     this.api.checkadmin()
@@ -35,50 +35,76 @@ export class SignupComponent implements OnInit {
     });
 
   }
+  test() {
+
+  }
   register(data): Observable<any> {
     const httpOptions = {
-      headers: new HttpHeaders({ 
-        'Access-Control-Allow-Origin':'*',
-        
+      headers: new HttpHeaders({
+        'Access-Control-Allow-Origin': '*',
+
       })
     };
     // let headers = new HttpHeaders();
     // headers = headers.set('Access-Control-Allow-Origin', '*').set('Content-Type', 'application/json')
     console.log(data)
-     return this.http.post(this.api.apiuser+`dangky`, data, httpOptions);
-     
+    return this.http.post(this.api.apiuser + `dangky`, data, httpOptions);
+
   }
   registerProces() {
 
+    var fullname = this.formGroup.controls['tenNguoiDung'].value;
 
+    const sdt = this.formGroup.controls['sDT'].value;
+    var specialChars = "<>@!#$%^&*()_+[]{}?:;|'\"\\,./~`-="
 
-    const password = this.formGroup.controls['matKhau'].value;
-    
-    const confirmPassword = this.formGroup.controls['xacNhanMatKhau'].value;
-
-
-    if (password != confirmPassword || password == "" || confirmPassword == "") {
-      alert("Password not match");
-      return;
+    var checkfullname = function (string) {
+      for (let i = 0; i < specialChars.length; i++) {
+        if (string.indexOf(specialChars[i]) > -1) {
+          return true
+        }
+      }
+      return false;
     }
 
 
-    else {
+    if (checkfullname(fullname) == true) {
+
+      return alert('Fullname không hợp lệ');
+    }
+    var checksdt = function phonenumber(string) {
+      var phoneno = /^\d{10}$/;
+      if(string.match(phoneno))
+        {
+           return true;
+        }
+      else
+        {
+     
+        return false;
+        }
+
+    }
+
+    if (checksdt(sdt) == false) {
+
+      return alert('Phone number không hợp lệ');
+    }
+
+     {
       if (this.formGroup.valid) {
         this.register(this.formGroup.value).subscribe((result) => {
 
           if (result)
            alert("Đăng kí thành công");
-          this.router.navigate(['/signin']);
+          this.router.navigate(['/']);
 
 
 
 
         }, error => {
 
-          if (error.error != "Dang ky that bai") {
-            alert("Emailhoặc user name này đã được đăng kí rồi !!");
-          }
+         alert(error.error.message)
         });
 
 
